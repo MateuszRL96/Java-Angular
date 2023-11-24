0aa3316594f62ad838f7fd422ed8f9ea8d2af006
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-11-25 00:36:46 +0100

    mega cool

--- stepbystep/demo/src/main/java/com/example/demo/controller/ProductController.java
+++ stepbystep/demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Product;
 import com.example.demo.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
@@ -15,22 +16,34 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public String getAllProducts(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "products";
+    }
+
+    @GetMapping("/add")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "add-product";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(@ModelAttribute Product product) {
+        productService.saveProduct(product);
+        return "redirect:/products";
     }
 
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id) {
-        return productService.getProductById(id).orElse(null);
+    public String getProductById(@PathVariable Long id, Model model) {
+        Product product = productService.getProductById(id).orElse(null);
+        model.addAttribute("product", product);
+        return "add-product";
     }
 
-    @PostMapping
-    public Product saveProduct(@RequestBody Product product) {
-        return productService.saveProduct(product);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id) {
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
+        return "redirect:/products";
     }
 }
--- stepbystep/demo/src/main/java/com/example/demo/entity/Product.java
+++ stepbystep/demo/src/main/java/com/example/demo/entity/Product.java
@@ -5,12 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int  id;
+    private Long  id;
     private String name;
-    private int price;
+    private Long price;
 }
